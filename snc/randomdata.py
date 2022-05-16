204712f67b1efa3945c1b9a98c8b402fb05bc3cf
arvestad <arve@math.su.se>
2022-05-16 16:00:58 +0200

    Actual outcome was not the intended, now fixed.

--- snc/randomdata.py
+++ snc/randomdata.py
@@ -10,18 +10,15 @@ def one_correlated_pair(i, j, subjects):
         scoring_line(f's{i}', f't{k}', score)
         scoring_line(f's{j}', f't{k}', score)
 
-def one_uncorrelated_pair(i, j, subjects):
+def one_uncorrelated(i, subjects):
     for k in subjects:
         score = random.uniform(0, 200)
         scoring_line(f's{i}', f't{k}', score)
-        score = random.uniform(0, 200)
-        scoring_line(f's{j}', f't{k}', score)
         
 
 def main():
     one_correlated_pair(0, 1, range(10))
-    one_uncorrelated_pair(0, 2, range(10,15))
-    one_uncorrelated_pair(1, 2, range(15,20))
+    one_uncorrelated(2, range(10))
 
 if __name__ == '__main__':
     main()
